70b0cec135afffcea3a7acbeab62fbb77da8d289
Istvan Acs <istvan.acs89@gmail.com>
2020-02-22 19:25:14 +0000

    Add incomeExpenses component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Header title='Expanse Tracker' />
       <div className='container'>
         <Balance />
+        <IncomeExpenses />
       </div>
     </React.Fragment>
   );
