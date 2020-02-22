8e9f1f00bb8e6ffbbeaf8bcedc5e4ba34e64a09e
Istvan Acs <istvan.acs89@gmail.com>
2020-02-22 19:23:18 +0000

    Create balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 
 import './App.css';
 
 function App() {
-  return <Header title='Expanse Tracker' />;
+  return (
+    <React.Fragment>
+      <Header title='Expanse Tracker' />
+      <div className='container'>
+        <Balance />
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
