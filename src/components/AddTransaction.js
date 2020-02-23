53301f8e3d7f2b0889e3d06e5c7f97b2d153952d
Istvan Acs <istvan.acs89@gmail.com>
2020-02-23 11:49:35 +0000

    Add addTransaction action to addtransaction component

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { generateID } from '../utils/utils';
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    const newTransaction = {
+      id: generateID(),
+      text,
+      amount: +amount
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <div className='form-control'>
           <label htmlFor='text'>Text</label>
           <input
